3d2608340deb3c6544aec0a5ccdddda3171685fb
Ardis Lu <ardisglu@gmail.com>
2021-08-06 18:24:10 -0700

    Add verification logic for custom short URLs

--- api/index.js
+++ api/index.js
@@ -1,3 +1,5 @@
+import { ethers } from 'ethers';
+
 async function handleRequest(request) {
   // Handle new short link creation
   if (request.method === 'POST') {
@@ -6,6 +8,9 @@ async function handleRequest(request) {
     const searchParams = new URLSearchParams(body);
     let shortUrl = searchParams.get('short-url'); // Must match HTML <input> ID
     const fullUrl = searchParams.get('full-url'); // Must match HTML <input> ID
+    const signature = searchParams.get('signature'); // Must match HTML <input> ID
+    let isCustomShortUrl = true;
+    const message = `Sign this message to create the short URL "https://alu.pm/${shortUrl}" for the full URL "${fullUrl}".`; // Must match frontend exactly
 
     // Validation regex to short circuit known bad inputs
     const validShortUrlRegex = /^[0-9a-zA-Z-_]*$/g;
@@ -13,9 +18,11 @@ async function handleRequest(request) {
     // Generic error responses
     const invalidShortUrlResponse = new Response('Invalid short URL', { status: 400 });
     const invalidFullUrlResponse = new Response('Invalid full URL', { status: 400 });
+    const invalidSignatureResponse = new Response('Invalid signature', { status: 403 });
 
     // Generate random 4 character shortUrl if none is provided
     if (shortUrl === null || shortUrl === '') {
+      isCustomShortUrl = false;
       do {
         let array = new Uint32Array(4);
         crypto.getRandomValues(array);
@@ -39,6 +46,41 @@ async function handleRequest(request) {
     }
     catch (error) {
       return invalidFullUrlResponse;
+    }
+
+    if (isCustomShortUrl) {
+      const signingAddress = ethers.utils.verifyMessage(message, signature);
+      // Data is hardcoded for speed to meet Cloudflare Worker CPU time limitation
+      // Hardcoded function name = balanceOf
+      // Hardcoded second uint256 parameter = 5
+      const data = `0x00fdd58e000000000000000000000000${signingAddress.substring(2)}0000000000000000000000000000000000000000000000000000000000000005`;
+
+      const rpcBody = {
+        "jsonrpc": "2.0",
+        "id": 1,
+        "method": "eth_call",
+        "params": [
+          {
+            "to": "0x74EE68a33f6c9f113e22B3B77418B75f85d07D22", // Hardcoded Zerion ERC-1155 NFT address
+            "data": data
+          },
+          "latest"
+        ]
+      }
+
+      // ethers.js is too slow, need to directly call the JSON RPC API
+      const resp = await fetch('https://cloudflare-eth.com', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(rpcBody)
+      });
+      const tokens = parseInt((await resp.json())['result']);
+
+      if (!(tokens > 0)) {
+        return invalidSignatureResponse;
+      }
     }
 
     await URL_MAPPING.put(shortUrl, fullUrl);

34206272086a0d62697a9bc323fa61e37598812f
Ardis Lu <ardisglu@gmail.com>
2021-09-16 00:47:30 -0700

    Update hardhat configuration and dependencies

--- alu-access-token/hardhat.config.js
+++ alu-access-token/hardhat.config.js
@@ -1,6 +1,31 @@
 /**
  * @type import('hardhat/config').HardhatUserConfig
  */
+
+require('dotenv').config()
+require('@nomiclabs/hardhat-ethers');
+
 module.exports = {
-  solidity: "0.8.6",
+  solidity: {
+    version: '0.8.6',
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 1000
+      }
+    }
+  },
+  defaultNetwork: 'localhost',
+  networks: {
+    rinkeby: {
+      url: 'https://rinkeby-light.eth.linkpool.io/',
+      accounts: { mnemonic: process.env.MNEMONIC },
+    }
+  },
+  paths: {
+    sources: "./contracts",
+    tests: "./test",
+    cache: "./cache",
+    artifacts: "./artifacts"
+  }
 };
